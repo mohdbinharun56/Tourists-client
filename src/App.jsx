f8047ee14fa8929bfddde8b6de61434e6ce0bd8c
mohdbinharun56 <arahaman461@gmail.com>
2025-10-28 12:16:39 +0600

    setup client

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-8xl'>Hello</div>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+export default App
